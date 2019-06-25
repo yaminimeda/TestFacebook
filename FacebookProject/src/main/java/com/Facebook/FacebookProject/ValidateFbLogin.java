fe649d4c257b11916986d6814e4c4511f2f38081
yaminimeda <yaminimeda533@gmail.com>
2019-06-25 23:19:01 +0530

    First Commit

---
+++ FacebookProject/src/main/java/com/Facebook/FacebookProject/ValidateFbLogin.java
@@ -0,0 +1,25 @@
+package com.Facebook.FacebookProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ValidateFbLogin {
+	
+	
+	WebDriver driver ;
+	@Test
+	public void launchApp()
+	{
+		driver=new ChromeDriver();
+driver.get("http://facebook.com");
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.manage().window().maximize();
+	driver.findElement(By.id("email")).sendKeys("7032096482");
+	driver.findElement(By.id("pass")).sendKeys("Sand@1234");
+	driver.findElement(By.id("u_0_a")).click();
+}
+}
